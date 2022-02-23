ca14d12d01227d2bcd2c8efa221754bab8051c9e
bug00 <fatoldsun00@gmail.com>
2022-02-23 07:49:13 +0100

    test son et accelerometre

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,4 @@
- import React from 'react';
+ import React, { useEffect } from 'react';
  import {
    SafeAreaView,
    ScrollView,
@@ -9,9 +9,55 @@
    View,
  } from 'react-native';
 
+ import {
+  accelerometer,
+  gyroscope,
+  setUpdateIntervalForType,
+  SensorTypes
+} from "react-native-sensors";
+import Sound from 'react-native-sound'
+import { map, filter } from "rxjs/operators";
+setUpdateIntervalForType(SensorTypes.accelerometer, 500); // defaults to 100ms
+
  const Home = () => {
    const isDarkMode = useColorScheme() === 'dark';
  
+   useEffect(()=>{
+    const subscription = accelerometer//.subscribe((e)=>console.log(e))
+    .pipe(map(({ x, y, z }) => x + y + z), filter(speed => speed > 12)).subscribe((e)=>{
+     
+      console.log(e)
+    })
+   
+    
+    return subscription.unsubscribe;
+   },[])
+
+
+
+// Enable playback in silence mode
+Sound.setCategory('Playback');
+
+// Load the sound file 'whoosh.mp3' from the app bundle
+// See notes below about preloading sounds within initialization code below.
+var whoosh = new Sound('whoosh.mp3', Sound.MAIN_BUNDLE, (error) => {
+  if (error) {
+    console.log('failed to load the sound', error);
+    return;
+  }
+  // loaded successfully
+  console.log('duration in seconds: ' + whoosh.getDuration() + 'number of channels: ' + whoosh.getNumberOfChannels());
+
+  // Play the sound with an onEnd callback
+  whoosh.play((success) => {
+    if (success) {
+      console.log('successfully finished playing');
+    } else {
+      console.log('playback failed due to audio decoding errors');
+    }
+  });
+});
+
    const backgroundStyle = {
      backgroundColor: isDarkMode ? 'black' : 'grey',
    };
@@ -22,10 +68,9 @@
        <ScrollView
          contentInsetAdjustmentBehavior="automatic"
          style={backgroundStyle}>
-         <Header />
          <View
            style={{
-             backgroundColor: isDarkMode ? Colors.black : Colors.white,
+             backgroundColor: isDarkMode ? 'black' : 'white',
            }}>
            <Text style={{color:'red'}}>TTTTTTTT</Text>
          </View>
